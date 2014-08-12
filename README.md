34d2f3915f1b9dcfbd07dfb12401bbeea0bbf2f5
Amoreena Hagedorn & Justin Pacubas <student@epicodus.com>
2014-08-12 16:48:01 -0700

    contain partially working user interface

---
+++ README.md
@@ -0,0 +1,16 @@
+#Train System
+by Amoreena Hagedorn and Justin Pacubas
+
+August 12, 2014
+
+##About
+
+Allows users to search train lines and stations to view when trains are coming. Also allows the user to add new stations and new lines, and pair them together.
+
+##Setup
+
+Either clone this repository or download the .zip and open in your console or sublime. You will have to create your own database.t
+
+##License
+
+MIT
--- train_system.rb
+++ train_system.rb
@@ -72,7 +72,7 @@ def train_menu
   puts "Press 'l' to list all train lines."
   puts "Press 'u' to update a train line."
   puts "Press 'd' to delete a train line."
-  puts "press 'x' to exit the program"
+  puts "press 'x' to exit the program."
 
   user_decision = gets.chomp
   if user_decision == 'a'
@@ -87,20 +87,71 @@ def train_menu
   elsif user_decision == 'u'
     update_train
   elsif user_decision == 'd'
-    main_menu
+    list_trains
+    puts "Enter the number of the train you would like to delete:"
+    user_decision = gets.chomp.to_i
+    Trains.delete_train(user_decision)
+    puts "Train deleted!"
+    train_menu
   elsif user_decision == 'x'
     puts "Thank you, please come again."
     exit
   else
     puts "Invalid entry. Please try again!"
-    main_menu
+    train_menu
   end
 end
 
 def stops_menu
+puts "*** Stops Menu ***"
+  puts "Press 'a' to add a stop."
+  puts "Press 'l' to list all stops."
+  puts "Press 'u' to update a stop."
+  puts "Press 'd' to delete a stop."
+  puts "press 'x' to exit the program."
+
+  user_decision = gets.chomp
+  if user_decision == 'a'
+    add_stop
+  elsif user_decision == 'l'
+    list_stops
+    puts "Press 'x' to return to stop_menu"
+    user_decision = gets.chomp
+    if user_decision == 'x'
+      stop_menu
+    end
+  elsif user_decision == 'u'
+    update_stop
+  elsif user_decision == 'd'
+    list_stops
+    puts "Enter the number of the stop you would like to delete:"
+    user_decision = gets.chomp.to_i
+    Stops.delete_stop(user_decision)
+    puts "Stop deleted!"
+    stops_menu
+  elsif user_decision == 'x'
+    puts "Thank you, please come again."
+    exit
+  else
+    puts "Invalid entry. Please try again!"
+    stops_menu
+  end
 end
 
-def station_menu
+def add_stop
+# puts "*** Add a Stop Menu ***"
+#   puts "Press 'a' to add a stop."
+#   puts "Press 'x' to return to the Stop Menu."
+
+#   user_decision = gets.chomp
+#   if user_decision == 'a'
+#     puts "Please enter in the new stopname:"
+#     user_line = gets.chomp
+#     new_stop = Trains.new({'lines' => user_line})
+#     new_stop.save
+#     puts "'#{new_stop.lines}' has been added."
+#     stop_menu
+#   end
 end
 
 def add_train
@@ -131,18 +182,96 @@ def update_train
   list_trains
   puts "Please enter the name of the train you would like to change:"
   user_decision = gets.chomp
-  Trains.all.each do |name|
-    if user_decision == name.lines
-      search_decision = Trains.search(user_decision)
-      puts "What would you like to re-name the train:"
-      new_name = gets.chomp
-      update = Trains.update_train(search_decision, new_name)
-      puts "The train is now called #{new_name}!"
-    else
-      puts "Your entry is invalid. Please try again!"
+  search_decision = Trains.search(user_decision)
+  if search_decision != nil
+    puts "What would you like to re-name the station:"
+    new_name = gets.chomp
+    Trains.update_train(search_decision, new_name)
+    puts "The train is now called #{new_name}!"
+    train_menu
+  else
+    puts "Your entry is invalid. Please try again!"
     update_train
-    end
   end
+  train_menu
+end
+
+def station_menu
+  puts "*** Station Menu ***"
+  puts "Press 'a' to add a station."
+  puts "Press 'l' to list all station."
+  puts "Press 'u' to update a station."
+  puts "Press 'd' to delete a station."
+  puts "press 'x' to exit the program."
+
+  user_decision = gets.chomp
+  if user_decision == 'a'
+    add_station
+  elsif user_decision == 'l'
+    list_stations
+    puts "Press 'x' to return to station_menu"
+    user_decision = gets.chomp
+    if user_decision == 'x'
+      station_menu
+    end
+  elsif user_decision == 'u'
+    update_station
+  elsif user_decision == 'd'
+    list_stations
+    puts "Enter the number of the station you would like to delete:"
+    user_decision = gets.chomp.to_i
+    Stations.delete_station(user_decision)
+    puts "Station deleted!"
+    station_menu
+  elsif user_decision == 'x'
+    puts "Thank you, please come again."
+    exit
+  else
+    puts "Invalid entry. Please try again!"
+    stations_menu
+  end
+end
+
+def add_station
+  puts "*** Add a Station Menu ***"
+  puts "Press 'a' to add a station."
+  puts "Press 'x' to return to the Station Menu."
+
+  user_decision = gets.chomp
+  if user_decision == 'a'
+    puts "Please enter in the new station name:"
+    user_station = gets.chomp
+    new_station = Stations.new({'stations' => user_station})
+    new_station.save
+    puts "'#{new_station.stations}' has been added."
+    station_menu
+  end
+end
+
+def list_stations
+  puts "Operating stations are:"
+  Stations.all.each do |station|
+    puts "#{station.id}. #{station.stations}"
+  end
+end
+
+def update_station
+  puts "*** Update Stations Menu ***"
+  list_stations
+  puts "Please enter the name of the station you would like to change:"
+  user_decision = gets.chomp
+  search_decision = Stations.search(user_decision)
+  if search_decision != nil
+    puts "What would you like to re-name the station:"
+    new_name = gets.chomp
+    Stations.update_station(search_decision, new_name)
+    puts "The station is now called #{new_name}!"
+    station_menu
+  else
+    puts "Your entry is invalid. Please try again!"
+    update_station
+  end
+  station_menu
 end
 
 main_menu
